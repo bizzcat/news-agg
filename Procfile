0bdc3f1f776ed1fde246689023c56dd4b8445c6b
Kyle Hubbard <khubb74@gmail.com>
2016-08-22 18:34:25 -0700

    prettify finished, deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn news-agg.wsgi --log-file -
--- newsAgg/settings.py
+++ newsAgg/settings.py
@@ -12,6 +12,24 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+
+# For Heroku Deployment
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
